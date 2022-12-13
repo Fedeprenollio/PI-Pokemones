2455e0d47dd40426fec133af0e4112f2ec6c8bc7
Federico <fedeprenollio@gmail.com>
2022-12-13 14:17:02 -0300

    arreglando titulo

--- client/src/componentes/LandinPage.jsx
+++ client/src/componentes/LandinPage.jsx
@@ -5,7 +5,7 @@ import s from "./landingPage.module.css"
 export default function LandingPage() {
   return (
     <div className={s.content}>
-      <h1 className={s.h1}>Bienvenidos a la pokeApi</h1>
+      <h1 className={s.h1}>Bienvenidos a la pokeApp</h1>
       <Link to="/home" >
       <button className={s.btn} >Ingresar</button>
       
